d41f6c1f1f8c1d736ff573912318ec050b39f760
DESKTOP-A953543\Frank <franklin.macias864@gmail.com>
2022-01-05 19:45:23 +0100

    Project 01 - Weather App

--- Project-01-WeatherApp/js/index.js
+++ Project-01-WeatherApp/js/index.js
@@ -32,8 +32,8 @@ function findWeather(e) {
             console.log(data);
             document.getElementById("place").textContent = data.name + ", " + data.sys.country;
             document.getElementById("current-temp").textContent = Math.floor(data.main.temp) + "°";
-            document.getElementById("min-temp").textContent = Math.floor(data.main.temp_min) + "°";
-            document.getElementById("max-temp").textContent = Math.floor(data.main.temp_max) + "°";
+            document.getElementById("min-temp").textContent = "Min temperature: " + Math.floor(data.main.temp_min) + "°";
+            document.getElementById("max-temp").textContent = "Max temperature: " + Math.floor(data.main.temp_max) + "°";
             document.getElementById("current-icon").src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
             document.getElementById("current-icon").alt = data.weather[0].description;
             document.getElementById("current-temp-description").textContent = data.weather[0].description;
