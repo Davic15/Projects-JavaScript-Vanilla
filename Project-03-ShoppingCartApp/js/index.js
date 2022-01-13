1c8ad67e9e4d73f472c0a6c1d17a555985bb7fb5
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-01-14 00:55:28 +0100

    Project 03 - Shopping cart

--- Project-03-ShoppingCartApp/js/index.js
+++ Project-03-ShoppingCartApp/js/index.js
@@ -4,7 +4,9 @@ const txtPasswordEl = document.getElementById("password");
 const txtStatusEl = document.getElementById("message");
 let userData = {};
 let userToken = 0;
-var myRequest = "store.html";
+let myRequest = "store.html";
+let postData = {};
+
 
 btnLoginEl.addEventListener("click", loginSite);
 txtUsernameEl.addEventListener("keydown", focusPassword);
@@ -14,21 +16,10 @@ txtPasswordEl.addEventListener("keydown", focusButton);
     
 */
 
-function focusButton(e) {
-    if (e.key == "Enter") {
-        btnLoginEl.focus();
-    }
-}
-
-function focusPassword(e) {
-    if (e.key == "Enter") {
-        txtPasswordEl.focus();
-    }
-}
-
 function loginSite() {
     if (checkInput()) {
         sendRequest();
+        userData = JSON.parse(localStorage.getItem("fake-store"));
         fetchLogin();
     }
 }
@@ -36,9 +27,7 @@ function loginSite() {
 function checkInput() {
     let flag = false;
     if (!txtUsernameEl.value || !txtPasswordEl.value){
-        txtStatusEl.textContent = "Type your Credentials before click login";
-        txtUsernameEl.value = "";
-        txtPasswordEl.value = "";
+        clearFields();
         flag = false;
     } else {
         txtStatusEl.textContent = "";
@@ -47,21 +36,67 @@ function checkInput() {
     return flag;
 }
 
+function clearFields() {
+    txtStatusEl.textContent = "Type your Credentials before click login";
+    txtUsernameEl.value = "";
+    txtPasswordEl.value = "";
+}
+
 function checkCredentials() {
     let flag = false;
-    console.log("hi")
     if ((userData.username === txtUsernameEl.value) && (userData.password === txtPasswordEl.value)) {
         flag = true;
     } else {
+        clearFields();
         flag = false;
-        txtStatusEl.textContent = "The credentials are not correct."
-        txtUsernameEl.value = "";
-        txtPasswordEl.value = "";
     }
     return flag;
 }
 
-function fetchLogin() {
+const fetchLogin = async () => {
+    /*if (checkCredentials()) {
+        postData = {
+            method:'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify({
+                username: "johnd",
+                password: "m38rmF$"
+                })
+            }
+        
+
+        
+    }*/
+    postData = {
+        method:'POST',
+        mode: 'cors',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: {
+            username: "johnd",
+            password: "m38rmF$"
+            }
+        }
+    try {
+        console.log("aa")
+        const res = await axios.post(`https://fakestoreapi.com/auth/login`, {postData});
+        console.log(res);
+            /*userToken = data.token;
+            console.log(userToken)*/
+            //window.location.href = myRequest;
+    }
+    catch (err) {
+        console.error(err)
+    }
+
+}
+
+//function fetchLogin() {
    /* if (checkCredentials()) {
         console.log("entre")
         const response = await fetch('https://fakestoreapi.com/auth/login',{
@@ -101,39 +136,31 @@ function fetchLogin() {
         })
     }*/
     
-}
-/*
-function fetchGetUser() {
-    fetch(`https://fakestoreapi.com/users/1`)
-        .then(res => {
-            if(!res.ok) {
-                throw Error("Error in the server");
-            } else {
-                return res.json();
-            }
-        })
-        .then(data => {
-            userData = data;
-            localStorage.setItem("fake-store", JSON.stringify(data))
-        })
-        .catch(err => {
-            console.log(err);
-        })
-    
-}*/
+//}
+
 
 const sendRequest = async () => {
     try {
-        const res = await axios.get(`https://fakestoreapi.com/users/1`, {onDownloadProgress: function (e) {
-            txtStatusEl.textContent = "Loading.."
-            // Do whatever you want with the native progress event
-        }})
-        
-        console.log(res.data)
-        userData = res.data;
-        localStorage.setItem("fake-store", JSON.stringify(res.data))
-        
-    } catch (err) {
+        const res = await axios.get(`https://fakestoreapi.com/users/1`, {
+            onDownloadProgress: function (progressEvent) {
+                progressEvent.loaded ? txtStatusEl.textContent = "Done, redirecting" : txtStatusEl.textContent = "Trying to log in";
+            }
+        })
+        localStorage.setItem("fake-store", JSON.stringify(res.data)); 
+    } 
+    catch (err) {
         console.error(err);
     }
-};
+}
+
+function focusButton(e) {
+    if (e.key == "Enter") {
+        btnLoginEl.focus();
+    }
+}
+
+function focusPassword(e) {
+    if (e.key == "Enter") {
+        txtPasswordEl.focus();
+    }
+}
