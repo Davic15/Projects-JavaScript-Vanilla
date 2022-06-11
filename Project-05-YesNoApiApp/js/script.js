9529151b4f7ecc26dc94cdb89dd702afe67b329d
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-06-11 18:29:08 +0200

    New project for portfolio, Yes/No API

---
+++ Project-05-YesNoApiApp/js/script.js
@@ -0,0 +1,13 @@
+// API
+const API_ENDPOINT = 'https://yesno.wtf/api';
+
+/**
+ * STEPS:
+ *
+ * 1. Create a fetchAnswer function and call the API
+ * 2. Output the API's response
+ * 3. Attach fetchAnswer to an event listener
+ * 4. Clear output after 3 seconds
+ * 5. Optional: add loading/error states
+ *
+ */
