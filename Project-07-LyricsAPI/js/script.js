95e0115443d50b50f6fae4bb97d7d154520246df
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-06-12 19:55:22 +0200

    New project for portfolio, Lyrics API

--- Project-07-LyricsAPI/js/script.js
+++ Project-07-LyricsAPI/js/script.js
@@ -1,18 +1,14 @@
-// !!! NOTE: By the time of releasing, the API and its documentation has changed a bit.
-// For the first step of the challenge you'll need a URL which isn't mentioned in their docs anymore.
-// Check the HINTS section on the challenge's page for instruction or the YouTube video.
-
 /**
- * STEPS
- * 1. Add an event listener to the form and the button
- * 2. Tie the event listener a function that GETs suggestions for the searched words and outputs it
- * 3. Build a list with the results where each item has a button "Get Lyrics" of that entry and read out the artist and song name of the list item that was clicked
- * 4. Create a function that GETs the requested song lyric and output the result
- * 5. Extend the app with disabled/loading/error states and adjust the UI accordingly
+ *  Algorithm
+ *  1) Add an event Listener to the form and the button.
+ *  2) Attach the event listener a function that GETs the results for the searched input.
+ *  3) Build a list with the results.
+ *  4) Cr4eate a function that GETs the requested lyrics.
+ *  5) Extend the app adding error/loading/disable handlers.
  */
 
 // API
-const endPoint = 'https://api.lyrics.ovh';//https://api.lyrics.ovh/suggest/:query
+const endPoint = 'https://api.lyrics.ovh';
 
 const searchForm = $('#search-form');
 const searchButton = $('#search-button');
