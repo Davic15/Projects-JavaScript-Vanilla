679525605c6654f8fa47adacd7dc4d6c64e97f2f
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-06-12 12:47:25 +0200

    New project for portfolio, Weather API

--- Project-06-WeatherApp/js/script.js
+++ Project-06-WeatherApp/js/script.js
@@ -14,27 +14,174 @@ class fetchForecastApi {
         this.baseApiUrl = 'https://api.openweathermap.org/data/2.5/weather';
         this.weatherAPI = '0a56dc1de13ff9072d70706723096a71';
     }
-    getLocation() {
-        $.getJSON(this.baseApiUrl, {q: 'Roma', appid: this.weatherAPI, units: 'metric'}).done(data => this.getWeatherData(data.id));
-        //$.getJSON(baseApiUrl, {q: 'Berlin', appid: weatherAPI, units: 'metric'}).done(data => console.log(data.id));
+    getLocation(query, callback) {
+        $.getJSON(this.baseApiUrl, {q: query, appid: this.weatherAPI, units: 'metric'}).done(data => callback(data)).fail(() => { callback(null) });
+        //$.getJSON(this.baseApiUrl, {q: query, appid: this.weatherAPI, units: 'metric'}).done(data => this.getWeatherData(data.id));
     }
 
-    getWeatherData(location) {
-        $.getJSON(this.baseApiUrl, {id: location, appid: this.weatherAPI, units: 'metric'}).done(data => console.log('Here is your weather', data))
+    getWeatherData(location, callback) {
+        $.getJSON(this.baseApiUrl, {id: location, appid: this.weatherAPI, units: 'metric'}).done(data => callback(data)).fail(() => { callback(null) });
     }
 }
 
 class coreDomElements {
-    
+    constructor() {
+        this.searchForm = $('#search-form');
+        this.errorBox = $('#error-box');
+        this.searchBox = $('#search-box');
+        this.loaderBox = $('#loader-box');
+        this.forecastBox = $('#forecast-box');
+    }
+
+    showForecast() {
+        this.hideError();
+        this.forecastBox.removeClass('d-none');
+        this.forecastBox.addClass('d-flex');
+    }
+
+    showLoader() {
+        this.loaderBox.removeClass('d-none');
+    }
+
+    hideLoader() {
+        this.loaderBox.addClass('d-none');
+    }
+
+    showSearch() {
+        this.searchBox.removeClass('d-none');
+        this.searchBox.addClass('d-flex');
+    }
+
+    hideSearchBox() {
+        this.searchBox.removeClass('d-flex');
+        this.searchBox.addClass('d-none');
+    }
+
+    showError(message) {
+        this.hideLoader();
+        this.showSearch();
+        this.errorBox.removeClass('d-none');
+        this.errorBox.addClass('d-block');
+        this.errorBox.html(`<p class="mb-0">${message}</p>`);
+    }
+
+    hideError() {
+        this.errorBox.addClass('d-none');
+    }
+}
+
+class displayForecast {
+    showTodaysForecastDetails({name, value, unit}) {
+        $(`#forecast-details`).append(`
+            <div class="d-flex justify-content-between">
+                <span class="font-weight-bolder">${name}</span>
+                <span>${value} ${unit}</span>
+            </div>
+        `)
+    }
+}
+
+class dataMiddleware {
+    constructor() {
+        this.displayForecast = new displayForecast();
+        this.coreDomElements = new coreDomElements();
+    }
+    gatherTodaysForecastDetails(data) {
+        return {
+            'feels like': {
+                value: data.feels_like,
+                unit: '°C'
+            },
+            humidity: {
+                value: data.humidity,
+                unit: '%'
+            },
+            wind: {
+                value: Math.round(data.speed),
+                unit: 'm/s'
+            },
+            'air pressure': {
+                value: data.pressure,
+                unit: 'mb'
+            },
+            'max temp': {
+                value: Math.round(data.temp_max),
+                unit: '°C'
+            },
+            'min temp': {
+                value: Math.round(data.temp_min),
+                unit: '°C'
+            }
+        }
+    }
+    prepareDataForDom(data) {
+        const { feels_like, humidity, pressure, temp, temp_max, temp_min } = data.main
+        const { speed } = data.wind
+        const todaysForecastDetails = this.gatherTodaysForecastDetails({
+            feels_like, humidity, pressure, temp, temp_max, temp_min, speed
+        });
+        this.prepareTodaysForecastDetails(todaysForecastDetails);
+        this.coreDomElements.hideLoader();
+        this.coreDomElements.showForecast();
+    }
+
+    prepareTodaysForecastDetails(forecast) {
+        $.each(forecast, (key, value) => {
+            this.displayForecast.showTodaysForecastDetails({
+                name: key.toUpperCase(),
+                value: value.value,
+                unit: value.unit
+            });
+        })
+    }
 }
 
 class requestController {
     constructor() {
         this.fetchForecastApi = new fetchForecastApi();
-        this.init();
+        this.coreDomElements = new coreDomElements();
+        this.dataMiddleware = new dataMiddleware();
+        this.registerEventListener();
     }
-    init() {
-        this.fetchForecastApi.getLocation()
+
+    showRequestInProgress() {
+        this.coreDomElements.showLoader();
+        this.coreDomElements.hideSearchBox();
+    }
+
+    getQuery() {
+        return $('#search-query').val().trim();
+    }
+
+    fetchWeather(query) {
+        this.fetchForecastApi.getLocation(query, (location) => {
+            if(!location || location.length === 0) {
+                this.coreDomElements.showError('Could not find this location, please try again.');
+                return;
+            }
+            this.fetchForecastApi.getWeatherData(location.id, (data) => {
+                if(!data) {
+                    this.coreDomElements.showError('Could not proceed with the request, please try again later.');
+                    return;
+                }
+                this.dataMiddleware.prepareDataForDom(data);
+            });
+        })
+    }
+
+    onSubmit() {
+        const query = this.getQuery();
+        if(!query) return;
+
+        this.showRequestInProgress();
+        this.fetchWeather(query)
+    }
+
+    registerEventListener() {
+        this.coreDomElements.searchForm.on('submit', (e) => {
+            e.preventDefault();
+            this.onSubmit();
+        });
     }
 }
 
