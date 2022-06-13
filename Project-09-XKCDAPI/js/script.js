822e0608846923d4ce7be4f2d695b9b8e8d17b33
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-06-13 17:23:00 +0200

    New project for portfolio, XKCD API

--- Project-09-XKCDAPI/js/script.js
+++ Project-09-XKCDAPI/js/script.js
@@ -1,13 +1,11 @@
 /**
- * STEPS:
- * 1. create a requestController class and define the needed variables for the API call
- * 2. Create a method that will get the current (last added) comic and set the currentComicsNumber and
- *    maxComicsNumber accordingly, call that method on load
- * 3. Register an event for the random comic number and add all the chain of event to display it
- * 4. Add Previous/Next, First/Last and get Comic by ID functionality to the app
- * 5. Adjust UI states accordingly
+ * Algorithm:
+ *  1) Create a requestController class and define the needed variables.
+ *  2) Create a method that will get the current (last added), and set variables with max and min comics.
+ *  3) Register an event for the random comic and display it.
+ *  4) Add Previous/Next, First/Last and get Comic by ID functionality to the app.
+ *  5) Adjust UI states accordingly.
  */
-
 
  class RequestController {
     constructor() {
