4ed0f23a2e652008966ecda565bffb898585a193
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2023-06-30 20:45:06 +0200

    Project done. Now testing and fixing bugs

--- Project-11-FrontendTest/scripts/data.js
+++ Project-11-FrontendTest/scripts/data.js
@@ -6,17 +6,23 @@ users.map((user) => {
     str += `
         <li class="cards_item">
             <div class="card">
-                <div class="card_image"><img src="https://picsum.photos/500/300/?image=5" alt="${user.name}"></div>
+                <div class="card_image">
+                
+                <img src="${
+                    user.gender === 'female'
+                        ? 'assets/images/female.jpg'
+                        : 'assets/images/male.jpg'
+                }" alt="${user.name}"></div>
                 <div class="card_content">
                     <h2 class="card_title">${user.name}</h2>
                     <p class="card_text">${user.phone}</p>
                     <!--<button class="btn card_btn">Read More</button>-->
-                    <a href="mailto:${user.email}" class="btn card_btn">send email</a>
+                    <a href="mailto:${
+                        user.email
+                    }" class="btn card_btn">send email</a>
                 </div>
             </div>
         </li>
     `;
     ulData.innerHTML = str;
 });
-
-console.log(ulData);
